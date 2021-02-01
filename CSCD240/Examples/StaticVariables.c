35f357e323a511beeda1d4e208a417e50916bd6e
StrangeRanger <strangeranger01@randomserver.xyz>
2021-02-01 10:55:55 -0800

    Add more example files from class

---
+++ CSCD240/Examples/StaticVariables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fun(); 
+
+int main(){
+	
+    printf("%d ", fun());	// prints 1
+	printf("%d ", fun());    //prints 2
+	
+	return 0;
+}
+
+int fun(){
+	static int count = 0;
+	count++;
+	return count;
+}
