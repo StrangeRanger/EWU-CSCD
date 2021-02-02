ef3b2df94124e801d9fa3cff9cb11d7e4b1a272d
StrangeRanger <strangeranger01@randomserver.xyz>
2021-02-01 16:04:56 -0800

    Complete Lab3 for CSCD240

---
+++ CSCD240/Lab3/countChar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAXSIZE 100
+
+int main(){
+    char ch;
+    int i;
+    char sentence[MAXSIZE];
+    int alphabetsCount = 0;
+
+    printf("Type a sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    printf("Type a character that you'd like to find the number of occurrences"
+           " in a sentence: ");
+    scanf(" %c", &ch);
+
+    for (i = 0; sentence[i] != '\n'; i++) {
+        if (sentence[i] == toupper(ch) || sentence[i] == tolower(ch)) {
+            alphabetsCount++;
+        }
+    }
+
+    printf("Character '%c' has a frequency of %d\n", ch, alphabetsCount);
+}
---
+++ CSCD240/Lab3/finalValues.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    int b = 4;
+    double c = ++a + b++;
+    size_t size = sizeof(c/a);
+
+    printf("Size of a: %d\n", a);
+    printf("Size of b: %d\n", b);
+    printf("Size of c: %lf\n", c);
+    printf("Size of size: %zu\n", size);
+}
