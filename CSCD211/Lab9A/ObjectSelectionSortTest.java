b461cbf1d206aac11b688a482a6fb84db1a965f9
StrangeRanger <strangeranger01@randomserver.xyz>
2021-03-12 15:18:38 -0800

    Complete Lab9A

---
+++ CSCD211/Lab9A/ObjectSelectionSortTest.java
@@ -0,0 +1,24 @@
+/** This program tests the selectionSort method in the ObjectSelectionSorter class. */
+public class ObjectSelectionSortTest {
+    public static void main(String[] args) {
+        // Create a String array with test values.
+        String[] people = {"David", "Abe", "Katherine", "Beth", "Jeff", "Daisy"};
+
+        // Display the array's contents.
+        System.out.println("Original order: ");
+        for (String element: people) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+
+        // Sort the array.
+        ObjectSelectionSorter.selectionSort(people);
+
+        // Display the array's contents.
+        System.out.println("\nSorted order: ");
+        for (String element: people) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
